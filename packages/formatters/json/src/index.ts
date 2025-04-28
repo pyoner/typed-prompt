72c3e460a9d649398c72836081b03d9878c55f5e
Askar Yusupov <devex.soft@gmail.com>
2025-04-28 08:30:53 +0500

    feat(json): create JSONFormatter class and use it in jsonFormatter function
    This change introduces a `JSONFormatter` class that encapsulates the
    formatting logic. The `jsonFormatter` function now instantiates and uses
    this class to perform the formatting. This change improves code
    organization and allows for easier customization of the JSON formatting
    process in the future.

--- packages/formatters/json/src/index.ts
+++ packages/formatters/json/src/index.ts
@@ -1,5 +1,17 @@
-import type { Format } from '@typed-prompt/types';
+import type { Format, Formatter } from '@typed-prompt/types';
+
+export class JSONFormatter implements Formatter {
+  constructor(
+    readonly name: string = 'prompt',
+    readonly spacer: string | number = 2,
+  ) {}
+
+  format(obj: unknown) {
+    return JSON.stringify({ [this.name]: obj }, null, this.spacer);
+  }
+}
 
 export const jsonFormatter: Format = (obj) => {
-  return JSON.stringify({ prompt: obj }, null, 2);
+  const formatter = new JSONFormatter();
+  return formatter.format(obj);
 };
