f76f3bf75aa7ee7681abdefd226fc2ac29d111c0
Askar Yusupov <devex.soft@gmail.com>
2025-04-30 11:24:56 +0500

    feat: add tsdown bundler and configuration
    This commit introduces tsdown as a bundler and includes its
    configuration file (tsdown.config.ts). Additionally, it removes
    the "allowJs" option from the tsconfig.json file.

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown/config';
+
+export default defineConfig({
+  entry: 'src/**/*.ts',
+  format: 'esm',
+  dts: {
+    isolatedDeclarations: true,
+  },
+  clean: true,
+  platform: 'neutral',
+});
